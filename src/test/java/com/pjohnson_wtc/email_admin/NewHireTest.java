72499786d10630daab6fb2072a138957e19fdf1c
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-11-26 11:21:28 +0000

    Added comments to NewHireTest class

--- src/test/java/com/pjohnson_wtc/email_admin/NewHireTest.java
+++ src/test/java/com/pjohnson_wtc/email_admin/NewHireTest.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 public class NewHireTest {
 
+	//Create test object
 	NewHire newHire;
 	
 	@Before
@@ -17,6 +18,7 @@ public class NewHireTest {
 		newHire.setPassword("OktaHatesMe");
 	}
 	
+	//Test getters for first name, last name and email
 	@Test
 	public void testGetFirstName() {
 		assertEquals("Bob", newHire.getFirstName());
@@ -32,6 +34,7 @@ public class NewHireTest {
 		assertEquals("bob.jones@accounting.company.com", newHire.getEmail());
 	}
 	
+	//Test getter and setter for alternate email
 	@Test
 	public void testGetAlternateEmail() {
 		assertEquals("bob.jones@house.co.uk", newHire.getAlternateEmail());
@@ -42,6 +45,8 @@ public class NewHireTest {
 		newHire.setAlternateEmail("bob.jones@home.com");
 		assertEquals("bob.jones@home.com", newHire.getAlternateEmail());
 	}
+	
+	//Test getter and setter for mailbox capacity
 	@Test
 	public void testGetMailboxCapacity_default() {
 		assertEquals(2000000, newHire.getMailboxCapacity());
@@ -63,6 +68,7 @@ public class NewHireTest {
 		assertEquals(2000000, newHire.getMailboxCapacity());
 	}
 	
+	//Test getter and setter for password
 	@Test
 	public void testGetPassword() {
 		assertEquals("OktaHatesMe", newHire.getPassword());
@@ -74,11 +80,13 @@ public class NewHireTest {
 		assertEquals("SaveMeITSupport!", newHire.getPassword());
 	}
 	
+	//Test showInfo() method
 	@Test
 	public void testShowInfo() {
 		assertEquals("Name: Bob Jones, Email: bob.jones@accounting.company.com, Mailbox Capacity: 2000000MB", newHire.showInfo());
 	}
 
+	//Tear down test object
 	@After
 	public void teardown() {
 		newHire = null;
