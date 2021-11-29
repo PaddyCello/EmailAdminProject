86f50ea61761777d45cd0c5243c8c39ba54ae049
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-11-29 15:45:30 +0000

    Created Admin class, reduced NewHire to skeleton and removed tests

---
+++ src/main/java/com/pjohnson_wtc/email_admin/Admin.java
@@ -0,0 +1,11 @@
+package com.pjohnson_wtc.email_admin;
+
+public class Admin {
+	
+	private String[] departments = {"sales", "development", "accounting"};
+	
+	public String[] getDepartments() {
+		return departments;
+	}
+	
+}
--- src/main/java/com/pjohnson_wtc/email_admin/NewHire.java
+++ src/main/java/com/pjohnson_wtc/email_admin/NewHire.java
@@ -6,16 +6,17 @@ public class NewHire {
 	//Instance variables - mailboxCapacity is initially given a default value
 	private String firstName;
 	private String lastName;
+	private String department; //Will need G's and S's
 	private String email;
 	private String alternateEmail;
 	private int mailboxCapacity = 2000000;
 	private String password;
 	
 	//Constructor - will be called from within Admin.createNewHire()
-	public NewHire(String firstName, String lastName, String email) {
+	public NewHire(String firstName, String lastName, String department) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.email = email;
+		this.department = department;
 	}
 	
 	//Necessary getters and setters
@@ -28,7 +29,11 @@ public class NewHire {
 		return lastName;
 	}
 	
-	public String getEmail() {
+	public String getDepartment() {
+		return department;
+	}
+	
+	public String getEmail() { //Add private generateEmail() and call from constructor
 		return email;
 	}
 
@@ -36,44 +41,44 @@ public class NewHire {
 		return alternateEmail;
 	}
 	
-	//Include validity check? (eg check for @ sign)
-	public void setAlternateEmail(String alternateEmail) {
-		this.alternateEmail = alternateEmail;
-	}
+	//Include validity check? (eg check for @ sign) (include as separate method in Admin class)
+//	public void setAlternateEmail(String alternateEmail) {
+//		this.alternateEmail = alternateEmail;
+//	}
 	
 	public int getMailboxCapacity() {
 		return mailboxCapacity;
 	}
-	public void setMailboxCapacity(int mailboxCapacity) {
-		
-		//Setter only updates mailboxCapacity if number is positive
-		//Would it be better to have this method throw an Exception if number is less than zero?
-		if (mailboxCapacity > 0) {
-			this.mailboxCapacity = mailboxCapacity;
-		} else {
-			System.out.println("Mailbox capacity must be greater than zero.");
-		}
-	}
+//	public void setMailboxCapacity(int mailboxCapacity) {
+//		
+//		//Setter only updates mailboxCapacity if number is positive
+//		//Would it be better to have this method throw an Exception if number is less than zero?
+//		if (mailboxCapacity > 0) {
+//			this.mailboxCapacity = mailboxCapacity;
+//		} else {
+//			System.out.println("Mailbox capacity must be greater than zero.");
+//		}
+//	}
 	
 	//Need getter for testing purposes, can remove after
 	public String getPassword() {
 		return password;
 	}
 	
-	public void setPassword(String password) {
-		this.password = password;
-	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
 	
 	//Password generator
-	public void generatePassword() {
-		//TODO
-	}
+//	public void generatePassword() {
+//		//TODO
+//	}
 	
 	//Method for displaying name, email and mailbox capacity
 	//May not be needed - project brief requested get methods, plural
-	public String showInfo() {
-		
-		return "Name: " + firstName + " " + lastName + ", Email: " + email + ", Mailbox Capacity: " + mailboxCapacity + "MB";
-	}
+//	public String showInfo() {
+//		
+//		return "Name: " + firstName + " " + lastName + ", Email: " + email + ", Mailbox Capacity: " + mailboxCapacity + "MB";
+//	}
 	
 }
