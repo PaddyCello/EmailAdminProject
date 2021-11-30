1f700ce4070be538b5a7c25c310fc6ced180d81e
Patrick Johnson <patrick.johnson@wundermanthompson.com>
2021-11-30 10:29:36 +0000

    Added logs and console handler to Main

--- src/main/java/com/pjohnson_wtc/email_admin/Main.java
+++ src/main/java/com/pjohnson_wtc/email_admin/Main.java
@@ -1,19 +1,38 @@
 package com.pjohnson_wtc.email_admin;
 
+import java.util.logging.*;
+
 public class Main {
+	
+	private static Logger logger = Logger.getAnonymousLogger();
+	private static Handler consoleHandler = new ConsoleHandler();
 
 	public static void main(String[] args) {
-		Admin admin = new Admin();
 		
-		String adamMinistrator = admin.createNewHire("Adam", "Ministrator", "Development");
-		String annGineer = admin.createNewHire("Ann", "Gineer", "Development");
-		String mikeRosoft = admin.createNewHire("Mike", "Rosoft", "Office");
+		logger.setLevel(Level.ALL);
+		consoleHandler.setLevel(Level.ALL);
+		logger.addHandler(consoleHandler);
 		
-		System.out.println(adamMinistrator);
-		System.out.println(annGineer);
+		logger.info("Starting log");
+		
+		try {
+			Admin admin = new Admin();
+			
+			String adamMinistrator = admin.createNewHire("Adam", "Ministrator", "Development");
+			String annGineer = admin.createNewHire("Ann", "Gineer", "Development");
+			String mikeRosoft = admin.createNewHire("Mike", "Rosoft", "Office");
+		
+			System.out.println(adamMinistrator);
+			System.out.println(annGineer);
 		
 		
-		System.out.println(admin.getAllNewHires());
+			System.out.println(admin.getAllNewHires());
+			
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Could not create object from constructor", e);
+		}
+		
+		logger.fine("Done");
 	}
 
 }
